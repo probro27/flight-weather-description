382ca2bcbfe96ac3671b6cc21d73f5b70f82746b
probro27 <prabhavkhera@hotmail.com>
2022-10-03 18:03:59 -0400

    Added support for location and time

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,10 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
+import airports from "airport-codes";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
 
 export default async function (req, res) {
     const completion = await openai.createCompletion({
@@ -17,8 +17,16 @@ export default async function (req, res) {
       presence_penalty: 0,
       best_of: 1
     });
-    console.log(completion.data);
-    res.status(200).json({ result: completion.data.choices[0].text });
+    const weatherCode = req.body.weatherCode;
+    const time = weatherCode.split(" ")[2].slice(2);
+    const dateUTC = new Date().toISOString();
+    const date = dateUTC.slice(0, 11);
+    const timeOfPublish = `${date}${time.slice(0, 2)}:${time.slice(2, 4)}:00Z`;
+    const fullTime = new Date(timeOfPublish);
+    const localTimeOfPublish = fullTime.toLocaleTimeString('en-US');
+    const iata = weatherCode.split(" ")[1].slice(1);
+    const airportName = airports.findWhere({ iata: iata }).get('name');
+    res.status(200).json({ airport: airportName, timeOfPublish: localTimeOfPublish, result: completion.data.choices[0].text });
   }
   
   function generatePrompt(weatherCode) {
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import styles from "./index.module.css";
 export default function Home() {
   const [weatherCodeInput, setWeatherCodeInput] = useState("");
   const [result, setResult] = useState();
+  const [timeOfPublish, setTimeOfPublish] = useState();
+  const [airportName, setAirportName] = useState("");
+  const [resultReceived, setResultReceived] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -17,6 +20,9 @@ export default function Home() {
     });
     const data = await response.json();
     setResult(data.result);
+    setTimeOfPublish(data.timeOfPublish);
+    setResultReceived(true);
+    setAirportName(data.airport);
     // setWeatherCodeInput("");
   }
 
@@ -40,7 +46,16 @@ export default function Home() {
           />
           <input type="submit" value="Generate description" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {
+          resultReceived && 
+            <div>
+              <div className={styles.result}>Weather Code: {weatherCodeInput}</div>
+              <div className={styles.result}>Localtion: {airportName}</div>
+               <div className={styles.result}>Time of Publish: {timeOfPublish}</div>
+               <div className={styles.result}>{result}</div>
+            </div>
+        }
+        
       </main>
     </div>
   );
